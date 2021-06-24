b72925d88eca470326bca637a902d9e287ff4ace
revants <60282587+revants@users.noreply.github.com>
2021-06-23 21:32:19 -0700

    Create nameGen.lua
    gens random names ig

---
+++ nameGen.lua
@@ -0,0 +1,21 @@
+local firstName = {
+    "ark",
+    "bank",
+    "mode",
+    "ghost",
+    "tri",
+    "ant",
+    "civ"
+}
+local secondName = {
+    "vert",
+    "mane",
+    "fulls",
+    "void"
+}
+local var1 = math.random(1,#firstName)
+local var2 = math.random(1,#secondName)
+local pval1 = firstName[v1]
+local pval2 = secondName[v2]
+
+rconsoleprint(tostring(pval1.. pval2))

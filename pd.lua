68ab1a0f76d19055389bd11c889396fa6108ce66
hazel <60282587+hazelvert@users.noreply.github.com>
2023-08-09 14:27:21 -0700

    Update pd.lua

--- pd.lua
+++ pd.lua
@@ -1,5 +1,3 @@
-repeat task.wait() until game:IsLoaded()
-task.wait(10)
 if 1 == 1 then
 	loadstring(game:HttpGet("https://raw.githubusercontent.com/hazelvert/releases/master/pdBackup.lua"))()
 	return
